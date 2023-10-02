17a4246b65e896b32f9b53d022dc0493eca980f8
jxwilliams11 <jxwilliams11@ualr.edu>
2023-10-02 09:23:36 -0500

    a

--- app/src/main/java/com/ualr/simpletasklist/MainActivity.java
+++ app/src/main/java/com/ualr/simpletasklist/MainActivity.java
@@ -1,17 +1,26 @@
 package com.ualr.simpletasklist;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
-
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.EditText;
 import com.ualr.simpletasklist.databinding.ActivityMainBinding;
 
+import com.ualr.simpletasklist.model.Task;
+import com.ualr.simpletasklist.model.TaskList;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
     // TODO 05. Add a TaskList member to the MainActivity. Initialize the new member.
+
+    HashMap<Integer, Task> map = new HashMap<>();
+    TaskList list = new TaskList(map);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +30,71 @@ public class MainActivity extends AppCompatActivity {
 
         //TODO 06.02 Bind the onAddBtnClicked method to the add button, so the onAddBtnClicked is
         // triggered whenever the user clicks on that button
+        onAddBtnClicked();
 
         //TODO 07.02 Bind the onDeleteBtnClicked method to the delete button, so that method is
         // triggered whenever the user clicks on that button
+        onDeleteBtnClicked();
 
         //TODO 08.02 Bind the onDoneBtnClicked method to the done button, so the onDoneBtnClicked method is
         // triggered whenever the user clicks on that button
+        onDoneBtnClicked();
     }
 
 
     // TODO 06. Create a new functionality to add a new task using the description provided
     //  through the text field on the top of the screen by tapping the "+" on the right
 
+
     // TODO 06.01. Create a new method called onAddBtnClicked.
+    protected void onAddBtnClicked()
+    {
+        ImageView button = (ImageView) findViewById(R.id.add_btn);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+        @Override
+        public void onClick(View view){
+            EditText edit = (EditText)findViewById(R.id.editTextTextPersonName);
+            list.addTask(String.valueOf(edit.getText()));
+            TextView text = (TextView) findViewById(R.id.taskList);
+            text.setText(list.toString());
+    }
+    });
+    }
+
+    protected void onDeleteBtnClicked()
+    {
+        com.google.android.material.button.MaterialButton button = findViewById(R.id.deleteBtn);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view){
+                EditText edit = (EditText)findViewById(R.id.editTextTaskId);
+                String x = String.valueOf(edit.getText());
+                Integer y = Integer.parseInt(x);
+                list.delete(y);;
+                TextView text = (TextView) findViewById(R.id.taskList);
+                text.setText(list.toString());
+            }
+        });
+    }
+
+    protected void onDoneBtnClicked()
+    {
+        com.google.android.material.button.MaterialButton button = findViewById(R.id.clearBtn);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view){
+                EditText edit = (EditText)findViewById(R.id.editTextTaskId);
+                String x = String.valueOf(edit.getText());
+                Integer y = Integer.parseInt(x);
+                list.markDone(y);
+                TextView text = (TextView) findViewById(R.id.taskList);
+                text.setText(list.toString());
+            }
+        });
+    }
 
     // TODO 06.05. Invoke TaskList class' add method to ask the TaskList to
     //  add a new Task with the description provided through the text field.
--- app/src/main/java/com/ualr/simpletasklist/model/TaskList.java
+++ app/src/main/java/com/ualr/simpletasklist/model/TaskList.java
@@ -1,5 +1,7 @@
 package com.ualr.simpletasklist.model;
 
+import java.util.HashMap;
+
 public class TaskList {
 
     // TODO 03. Define TaskList's attributes. The class will have just one attribute to store all
@@ -8,19 +10,59 @@ public class TaskList {
     // TIP. We need a data structure able to dynamically grow and shrink. That's why we'll use a HashMap.
     // where keys will be integer values and the mapped values will be a task object
 
+    HashMap<Integer, Task> createdTasks = new HashMap<Integer, Task>();
+    Integer id = 0;
     // TODO 04. Define the class constructor and the corresponding getters and setters.
+
+    public TaskList(HashMap<Integer, Task> map){
+        createdTasks = map;
+    }
+
+    public HashMap<Integer, Task> getMapValue(){
+        return createdTasks;
+    }
+
+    public Task getTask(Integer taskID){
+        return createdTasks.get(taskID);
+    }
 
     // TODO 06.03. Define a new method called "add" that, given a task description, will create a
     //  new task and add it to the task list.
+
+    public void addTask(String description)
+    {
+        Task task = new Task(description, false);
+        createdTasks.put(id, task);
+        id++;
+    }
 
     // TODO 06.04. Define a new "toString" method that provides a formatted string with all the tasks in the task list.
     // Format: 1 line per task. Each line should start with the id number of the task, then a dash, and the task description right after that.
     // If the task is marked as done, "Done" should be included at the end of the line
 
+    public String toString()
+    {
+        StringBuilder x = new StringBuilder();
+
+        for(HashMap.Entry<Integer, Task> entry:  createdTasks.entrySet()){
+            x.append(entry.getKey());
+            x.append(" - ");
+            x.append(entry.getValue().getTaskDescription());
+            if(entry.getValue().getTaskIsDone())
+            { x.append(" - Done");}
+            x.append("\n");
+        }
+        return x.toString();
+    }
+
     // TODO 07.03. Define a new method called "delete" that, given a task id, will delete the
     //  corresponding task from the task list.
 
+    public void delete(Integer taskID){createdTasks.remove(taskID);}
+
     // TODO 08.03. Define a new method called "markDone" that, given a task id, will mark the
     //  corresponding task as done.
+
+    public void markDone(Integer taskID){getTask(taskID).setTaskIsDone();}
 
 }
